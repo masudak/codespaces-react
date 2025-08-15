715249a7be6e022dc1ffcc579cf4df2bac26c1f2
masudak <738547+masudak@users.noreply.github.com>
2025-08-15 15:12:23 +0000

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import liff from "@line/liff";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
   const [name, setName] = useState("");
 
   useEffect(() => {
@@ -11,15 +13,29 @@ function App() {
         liffId: import.meta.env.VITE_LIFF_ID
       })
       .then(() => {
+        setMessage("LIFF init succeeded.");
         liff.getProfile()
           .then((profile) => {
             setName(profile.displayName);
           })
+          .catch((err) => {
+            console.log("error", err);
+          });
       })
-  }, []);
-  
+      .catch((e) => {
+        setMessage("LIFF init failed.");
+        setError(`${e}`);
+      });
+  });
+
   return (
     <div className="App">
+      {message && <p>{message}</p>}
+      {error && (
+        <p>
+          <code>{error}</code>
+        </p>
+      )}
       {name && <p>こんにちは、{name}さん</p>}
     </div>
   );
